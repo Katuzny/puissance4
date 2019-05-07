60310d5baab51a59525b2f6c54996be502fe21de
katuzny <katuznychristophe@gmail.fr>
2019-05-07 12:28:22 +0200

    js2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var numero de joueur = 1;
+var numerojoueur = 1;
 var nbcolonnes = 5;
 var nblignes = 5;
 var jeu = true;
@@ -6,8 +6,32 @@ var texte ="";
 var plateau=[];
 
 for (var i = 0; i < nblignes; i++) {
-	plateau[i]=[];
+	plateau[i] = new Array();
 }
 function newgame() {
 	console.log(newgame);
+}
+
+function newgame() {
+	for (var i = 0; i < nblignes; i++) {
+	
+		for (var j = 0; j < nbcolonnes; j++) {
+		plateau[i][j]=[0];
+		}
+	}
+	joueur =1;
+	jeu()=true;
+}
+function affichetexteannonce(argument= "le jeu commence ! c'est au tour du joueur")=nomjoueur(joueur) {
+	innerhtml = get element id = affichetexteannonce()
+}
+function nomjoueur(numerojoueur()) {
+	
+	if (numerojoueur()==1){
+		return ("rouge")
+
+	}
+	else{
+		return ("bleu")
+	}
 }
