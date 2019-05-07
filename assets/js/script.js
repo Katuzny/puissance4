b402e56a1a647800d814ece00415a62424dd905e
katuzny <katuznychristophe@gmail.fr>
2019-05-07 12:00:33 +0200

    script

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var numero de joueur = 1;
+var nbcolonnes = 5;
+var nblignes = 5;
+var jeu = true;
+var texte ="";
+var plateau=[];
+
+for (var i = 0; i < nblignes; i++) {
+	plateau[i]=[];
+}
+function newgame() {
+	console.log(newgame);
+}
